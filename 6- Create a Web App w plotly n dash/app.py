435f08e54136414c9a363e32feccd7f83cff8543
Andrew <andtas@gmail.com>
2021-12-27 16:19:49 -0500

    added results data from 2020 and 2021

--- 6- Create a Web App w plotly n dash/app.py
+++ 6- Create a Web App w plotly n dash/app.py
@@ -4,36 +4,38 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 
-def get_stats_df():
+def get_df(table):
     import sqlite3
     import pandas as pd
-    sql='select * from playerstats'
+    sql=f'select * from {table}'
     con = sqlite3.connect('C://Users//rucku//Desktop//Coding Projects//mosconi//players.db')
-    stats = pd.read_sql(sql,con,index_col='index')
-    #fix index to start at 1 rather than 0
-    stats.index = pd.RangeIndex(start=1, stop=max(stats.index)+2, step=1)
-    return stats
+    df = pd.read_sql(sql,con,index_col='index')
+    df.index = pd.RangeIndex(start=1, stop=max(df.index)+2, step=1)  #make index start at 1
+    return df
 
-df = get_stats_df()
-all_stats = df.columns.to_list()[2:]
-
+playerstats_df = get_df('playerstats')
+stat_list = playerstats_df.columns.to_list()[2:]
 
 app = dash.Dash(__name__)
 
-
 app.layout = html.Div([
+     html.H1(children='Use the dropdowns to select x and y axes', style={
+        'textAlign': 'center'
+    }),
     dcc.Dropdown(
         id="dropdown",
         options=[{"label": x, "value": x} 
-                 for x in all_stats],
-        value=all_stats[0],
+                 for x in stat_list],
+        value=stat_list[0],
+        placeholder='x',
         multi=False
     ),
     dcc.Dropdown(
         id="dropdown2",
         options=[{"label": x, "value": x} 
-                 for x in all_stats],
-        value=all_stats[1],
+                 for x in stat_list],
+        value=stat_list[1],
+        placeholder='y',
         multi=False
     ),
     dcc.Graph(id="splom"),
@@ -44,7 +46,7 @@ app.layout = html.Div([
     Output("splom", "figure"), 
     [Input("dropdown", "value"),Input("dropdown2", "value")])
 def update_chart(x,y):
-    fig = px.scatter(data_frame= df, x=x,y=y,text='Player')
+    fig = px.scatter(data_frame= playerstats_df, x=x,y=y,text='Player')
     return fig
 
 
--- scripts/my_plotly_code.py
+++ scripts/my_plotly_code.py
@@ -5,8 +5,12 @@ import pandas as pd
 import plotly.express as px
 import sqlite3
 
-def get_stats_df():
-    sql='select * from playerstats'
+def get_df(table):
+    '''
+    Returns df corresponding to the table in df. Tables include: 
+    Playerlists, Winning_player, mvp, players, playerstats, results, rookies, rosters, yearly
+    '''
+    sql=f'select * from {table}'
     con = sqlite3.connect('C://Users//rucku//Desktop//Coding Projects//mosconi//players.db')
     stats = pd.read_sql(sql,con,index_col='index')
     #fix index to start at 1 rather than 0
@@ -21,11 +25,19 @@ def top_n(df, n = 10, stat_col = 'Cwin'):
     frame.index= pd.RangeIndex(start=1, stop=len(frame)+1, step=1)
     fig = px.scatter(data_frame=frame,x='Cmp',y='Cwin',text='Player')
     return frame, fig
-    
 
+def add_state_codes(df, left_on = 'Location'):
+    '''
+    Returns new df with state codes added on, especially useful for creating chloropleth maps
+    '''
+    f = pd.read_pickle('C:\\Users\\rucku\\Desktop\\Coding Projects\\mosconi\\4- Data Visualization\\state_codes.pkl')
+    f = f[['State/District','Postal Code']]
+    df = pd.merge(df, f ,left_on = 'Location', right_on = 'State/District')
+    df.drop('State/District', axis = 1, inplace = True)
+    return df
 
 def main():
-    stats = get_stats_df()
+    stats = get_df('playerstats')
     frame, fig = top_n(df = stats, n = 10, stat_col = 'Cpct')
     fig.show()
 
